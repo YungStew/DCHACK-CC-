767e107e9278256a9a3e0b0289d8fcd17ab6f5e0
KelleytinaVW <kelleytina.williams@gmail.com>
2018-02-10 17:32:32 -0500

    kt 2

--- core/src/com/hampton/game/PlayHO.java
+++ core/src/com/hampton/game/PlayHO.java
@@ -1,17 +1,19 @@
 package com.hampton.game;
 
-import com.badlogic.gdx.Application;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.audio.Music;
-import com.badlogic.gdx.audio.Sound;
-import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.scenes.scene2d.Actor;
-import com.badlogic.gdx.scenes.scene2d.InputEvent;
-import com.badlogic.gdx.scenes.scene2d.ui.Label;
-import com.badlogic.gdx.scenes.scene2d.utils.ActorGestureListener;
-import com.hampton.game.GameScreen;
-import com.hampton.game.utils.ActorUtils;
+package com.hampton.game.demo;
+
+        import com.badlogic.gdx.Gdx;
+        import com.badlogic.gdx.audio.Music;
+        import com.badlogic.gdx.audio.Sound;
+        import com.badlogic.gdx.graphics.Color;
+        import com.badlogic.gdx.math.MathUtils;
+        import com.badlogic.gdx.math.Vector2;
+        import com.badlogic.gdx.scenes.scene2d.Action;
+        import com.badlogic.gdx.scenes.scene2d.Actor;
+        import com.badlogic.gdx.scenes.scene2d.InputEvent;
+        import com.badlogic.gdx.scenes.scene2d.utils.ActorGestureListener;
+        import com.hampton.game.GameScreen;
+        import com.hampton.game.utils.ActorUtils;
 
 /**
  * Created by MichaelS on 2/10/2018.
